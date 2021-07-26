e11456a3a15b52666ef406daca1e65af83739cb3
Panhao Wu <wupanhao@qq.com>
2021-07-26 03:58:51 +0100

    image speed adjustable

--- catkin_ws/src/pi_cam/src/camera_node.py
+++ catkin_ws/src/pi_cam/src/camera_node.py
@@ -22,6 +22,7 @@ import os
 import cv2
 import numpy as np
 import time
+import threading
 
 
 class CameraNode(object):
@@ -51,8 +52,9 @@ class CameraNode(object):
         self.image_msg = None  # Image()
         self.compressed_msg = None  # CompressedImage()
         self.last_publish = 0
+        self.update_frequence = 15
         self.last_update = time.time()
-        self.pub_raw = rospy.Publisher("~image_raw", Image, queue_size=1)
+        # self.pub_raw = rospy.Publisher("~image_raw", Image, queue_size=1)
 
         # self.pub_rect = rospy.Publisher("~image_rect", Image, queue_size=1)
         self.pub_camera_info = rospy.Publisher(
@@ -72,6 +74,8 @@ class CameraNode(object):
         rospy.Service('~list_cali_file', GetStrings, self.srvListCaliFile)
         rospy.Service('~load_cali_file', SetString, self.srvLoadCaliFile)
         rospy.Service("~set_camera_info", SetCameraInfo, self.srvSetCameraInfo)
+        rospy.Service('~set_update_frequence', SetInt32,
+                      self.srvSetUpdateFrequence)
         # rospy.Subscriber('~shutdown', Empty, self.cbShutdown)
         # self.pid_set_enable_srv = rospy.Service('~pid_set_enable', SetPid, self.cbPidSetEnable)
         # self.pub_image_detection = rospy.Publisher("~image_detections", Image, queue_size=1)
@@ -83,7 +87,11 @@ class CameraNode(object):
         else:
             self.pub_image = None
 
-        rospy.Timer(rospy.Duration.from_sec(1.0/20), self.cbPublish)
+        # reader2 = threading.Thread(target=self.start_update_loop)
+        # reader.daemon = True
+        # reader2.start()
+
+        self.timer = rospy.Timer(rospy.Duration.from_sec(1.0/self.update_frequence), self.cbPublish)
         rospy.loginfo("[%s] Initialized......" % (self.node_name))
 
     def srvCameraSetEnable(self, params):
@@ -205,6 +213,26 @@ class CameraNode(object):
             return SetStringResponse('加载成功')
         except Exception as e:
             return SetStringResponse('加载失败')
+
+    def srvSetUpdateFrequence(self, params):
+        print(params)
+        if params.value >= 1 and params.value <= 30:
+            self.update_frequence = params.value
+            self.timer.shutdown()
+            self.timer = rospy.Timer(rospy.Duration.from_sec(1.0/self.update_frequence), self.cbPublish)
+            return SetInt32Response(1, params.value)
+        else:
+            return SetInt32Response()
+
+    # not accurate
+    def start_update_loop(self):
+        # print(self.d51_driver.value_updated)
+        while not self.is_shutdown:
+            self.cbPublish()
+            if self.update_frequence == 0:
+                time.sleep(1)
+            else:
+                time.sleep(1.0/self.update_frequence)
 
 
 if __name__ == '__main__':
